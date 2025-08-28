d2bc5f610a129626fbbe9d0dae02cc20b8f99991
anuragmahish <anuragmahish.18o1@gmail.com>
2025-08-28 11:00:58 +0530

    Commit 1
    Signed-off-by: anuragmahish <anuragmahish.18o1@gmail.com>

---
+++ src/main/java/com/SaaS_School_Website/Main/MainApplication.java
@@ -0,0 +1,17 @@
+package com.SaaS_School_Website.Main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(MainApplication.class, args);
+
+        System.out.println("Running");
+        System.out.println("Running");
+	}
+
+}
